f97178009068a2364d5093ac1886b0deda4ccd30
Bank5807 <TAnWiPaSa@gmail.com>
2023-08-07 19:20:55 +0700

    first commit. partially finishs

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,11 +1,17 @@
 "use client";
 
+import { useRouter } from "next/router";
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
 export default function SearchResultPage() {
+  const router = useRouter();
+  const { searchInput } = router.query;
+
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  // const processedSearchInput = ...
+  const processedSearchInput = searchInput
+    ? searchInput.replaceAll("%20", " ")
+    : "";
 
   /*
   tip2 : Use "includes" string method to check substring
@@ -17,14 +23,22 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+  const filteredMovies = movieDB.filter((movie) =>
+    movie.title.toLowerCase().includes(processedSearchInput.toLowerCase())
+  );
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching "{processedSearchInput}"
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
+      </p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie) => (
+        <MovieRow key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 }
